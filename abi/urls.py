95aea77d954db80f2e934323f0bd9b606a9352de
kaique-psantos <kaique212pereira@gmail.com>
2024-10-24 20:51:15 -0300

    Adicionado arquivo Teste.html

--- abi/urls.py
+++ abi/urls.py
@@ -4,5 +4,6 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-  path(''       , views.index,  name='index'),
+  path('', views.index,  name='index'),
+  path('teste/', views.teste,  name='teste'),
 ]
--- abi/views.py
+++ abi/views.py
@@ -14,3 +14,10 @@ def index(request):
     'segment'  : 'index',
   }
   return render(request, "pages/index.html", context)
+
+def teste(request):
+
+  context = {
+    'segment'  : 'teste',
+  }
+  return render(request, "pages/teste.html", context)
