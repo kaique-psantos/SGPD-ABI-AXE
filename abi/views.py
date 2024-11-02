5804046e7742ed513d88452bf9d6bdb6daadf03f
kaique-psantos <kaique212pereira@gmail.com>
2024-11-02 17:22:54 -0300

    Removido dropdown do navigation e notificações

--- abi/views.py
+++ abi/views.py
@@ -8,16 +8,4 @@ def index(request):
   }
   return render(request, "pages/index.html", context)
 
-def teste(request):
 
-  context = {
-    'segment'  : 'teste',
-  }
-  return render(request, "pages/teste.html", context)
-
-def cargo(request):
-
-  context = {
-    'segment'  : 'cargo',
-  }
-  return render(request, "pages/cargo.html", context)
